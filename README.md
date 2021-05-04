5c2fb2d928ebde0a6358f11401e33962e8cd1d3d
karsten darden <kdarden45>
2021-05-04 15:10:33 -0500

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Chicago Art Wayfinder
+
+A cool page where you can see art from the Chicago Art Institute and learn more about it!
+
+## Description
+
+This page displays artwork from the Art Institute of Chicago and related information from a wikipedia page based on that art work.
+
+
+![A screenshot of the web page]()
+
+## Link to Deployed Website
+
+You can find a link to this app here: []()
+
+## Authors
+
+Molly Wilson, Chris Winters, Karsten Darden, Joshua Wood
---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+
+$('.dropdown-trigger').dropdown({
+  coverTrigger: false,
+  constrainWidth: false
+});
+
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+
+
+
+var loadFavorite = function () {
+  var savedFavorites = localStorage.getItem("favoriteArt");
+  if (!savedFavorites) {
+    return [];
+  }
+  savedFavorites = JSON.parse(savedFavorites);
+  return savedFavorites;
+}
+
+
+var saveFavorite = function (artwork) {
+var savedFavorites = loadFavorite();
+savedFavorites.push(artwork);
+localStorage.setItem("favoriteArt",JSON.stringify(savedFavorites));
+
+}
+
+const someArt = [
+  {
+      artId: 1,
+      name: "painting",
+      artist: "bob ross"
+  },
+  {
+      artId: 3,
+      name: "drawing",
+      artist: "little billy"
+  }
+]
+​
+saveFavorite(someArt[0]);
