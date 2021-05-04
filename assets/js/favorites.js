c720bf54f8ac2720ceb5a9a62ddcf161621eef61
Molly Wilson <molly_wilson1@outlook.com>
2021-05-04 11:07:46 -0500

    function for populating a single favorite card and function for displaying all favorite cards

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -8,6 +8,7 @@ const loadFavorites = function () {
 };
 
 const deleteFavorite = function (artwork) {
+    $("#" + artwork.id).remove();
     const savedFavorites = loadFavorites();
 
     const newFavorites = savedFavorites.filter(function (savedArtwork) {
@@ -37,6 +38,29 @@ const isFavorited = function (artwork) {
 };
 
 const displayFavorites = function () {
-    
+    let favorites = loadFavorites();
+    $("#favorites").empty();
+
+    if (favorites.length) {
+        $("#favorites").append("<div class='row title'><h4>Favorites:</h4></div>");
+        $("#favorites").append("<div class='row content'></div>");
+        
+        const row = $("#favorites").find(".content");
+        for (let favorite of favorites) {
+            row.append(populateFavoriteCard(favorite));
+        };
+        
+    }
 }
 
+const populateFavoriteCard = function (artwork) {
+    const template = $($("#favorite-card-template").html());
+    //give id to find for removal later
+    template.find("#card").parent().attr("id", artwork.id);
+    template.find("img").attr("src", artwork.imageUrl);
+
+    template.on("click", function () {
+        //show modal
+    });
+    return template;
+};
